ffa511abfd6c21b4d6034772b73567f81fbf5f9f
AYAKA <115390195+kamisatayaka@users.noreply.github.com>
2025-01-14 18:52:54 +0800

    上传
    上传捏

---
+++ Untitled1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+using namespace std;
+int main() {
+    ofstream outFile("numbers.txt");
+    if (!outFile) {
+        cerr << "Error opening file for writing." << endl;
+        return 1;
+    }
+    for (int i = 1; i <= 20; i++) {
+        double squareRoot = sqrt(i);
+        outFile << i << " " << squareRoot << endl;
+    }
+    outFile.close();
+	int numToSearch;
+	cout << "输入一个1到20之间的数: ";
+	cin >> numToSearch;
+	ifstream inFile("numbers.txt");
+    if (!inFile) {
+    	cerr << "Error opening file for reading." << endl;
+        return 1;
+    }
+    bool found = false;
+	string line;
+    while (getline(inFile, line)) {
+        int num;
+        double squareRoot;
+        istringstream iss(line);
+        iss >> num >> squareRoot;
+        if (num == numToSearch) {
+            cout << numToSearch << "的平方根是" << squareRoot << endl;
+            found = true;
+            break;
+        }
+    }
+    if (!found) cout << "The number you entered was not found in the file." << endl;
+    inFile.close();
+    return 0;
+}
+
